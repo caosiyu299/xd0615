57c4f3bccb754a02e8fae1124a84791de321340c
caosiyu299 <1651064741@qq.com>
2020-06-22 17:39:10 +0800

    66666

---
+++ lesson6/lcd.c
@@ -0,0 +1,21 @@
+
+#include <LiquidCrystal.h>
+
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+
+  lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+  lcd.print("wo shi xian nv");
+   lcd.setCursor(0,1);
+ lcd.print("wo zhi dao");
+ 
+}
+
+void loop() {
+ 
+  lcd.setCursor(0, 1);
+}
+ 
